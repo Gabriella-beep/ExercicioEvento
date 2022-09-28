50c76b20986ee5d3626befea9ea81d082431ab6d
Gabriella-beep <112718474+Gabriella-beep@users.noreply.github.com>
2022-09-28 15:08:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function pintarDeVermelho(){
+    this.style.backgroundColor = "red";
+ }
+
+function pintarDePreto(){
+    this.style.backgroundcolor = "black";
+}
+
+
+if (backgroundcolor = "black"){
+document.querySelector('.quadrado').addEventListener('click', pintarDeVermelho)
+} else{
+    document.querySelector('.quadrado').addEventListener('click', pintarDePreto)
+}
+
